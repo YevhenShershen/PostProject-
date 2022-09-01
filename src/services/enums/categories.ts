31074a2d054b65beb4f04b31400ef14f0de11b39
Yevhen <shershen.yevhen@gmai>
2022-09-01 20:17:35 +0200

    add some comments in categories.ts

--- src/services/enums/categories.ts
+++ src/services/enums/categories.ts
@@ -1,3 +1,6 @@
+//import categories from "@/services/enums/categories"; так импортируется
+//categories.POPULAR_ADDED_CATEGORIES а так вызывается
+
 enum categories {
   "LAST_ADDED_CATEGORIES" = "recent",
   "POPULAR_ADDED_CATEGORIES" = "viewed",
