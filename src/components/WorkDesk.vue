85cd510f35b996d914c10ff35d09daa0727abaaa
Yevhen <shershen.yevhen@gmai>
2022-09-01 20:17:03 +0200

    remove lines of code in WorkDesk.vue

--- src/components/WorkDesk.vue
+++ src/components/WorkDesk.vue
@@ -15,59 +15,7 @@ import AddPerson from "@/components/workdesk/AddPerson.vue";
     AddPerson,
   },
 })
-export default class WorkDesk extends Vue {
-  protected person = {
-    name: "",
-    surname: "",
-    area: "",
-    isHaveArea: true,
-    isActive: true,
-    extraHours: "",
-  };
-  dialog = false;
-  valid = true;
-  name = "";
-  surname = "";
-  nameRules = [
-    (v) => !!v || "Name is required",
-    (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
-  ];
-  surnameRules = [
-    (v) => !!v || "Surname is required",
-    (v) => (v && v.length <= 10) || "Surname must be less than 10 characters",
-  ];
-  area = "";
-  items = ["Item 1", "Item 2", "Item 3", "Item 4"];
-  checkbox = false;
-
-  validate() {
-    (this.dialog = false),
-      (this.person = {
-        name: this.name,
-        surname: this.surname,
-        area: this.area,
-        isHaveArea: true,
-        isActive: true,
-        extraHours: "",
-      });
-    this.$store.state.staffInformation.push(this.person);
-    this.person = {
-      name: "",
-      surname: "",
-      area: "",
-      isHaveArea: true,
-      isActive: true,
-      extraHours: "",
-    };
-    this.$refs.form.validate();
-  }
-  reset() {
-    this.$refs.form.reset();
-  }
-  resetValidation() {
-    this.$refs.form.resetValidation();
-  }
-}
+export default class WorkDesk extends Vue {}
 </script>
 
 <style scoped lang="scss"></style>
