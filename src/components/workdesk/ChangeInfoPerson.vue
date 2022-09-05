895b68385ccf089aaf587d11419ac7d433e2e013
Yevhen <shershen.yevhen@gmai>
2022-09-05 20:48:41 +0200

    sorting in the  component changeInfoPerson.vue

--- src/components/workdesk/ChangeInfoPerson.vue
+++ src/components/workdesk/ChangeInfoPerson.vue
@@ -1,90 +1,144 @@
 <template>
-  <v-col cols="3">
+  <v-col cols="6" class="change-info">
     <h2 class="mt-4">Change information</h2>
-    <div
-      v-for="(person, index) in $store.state.staffInformation"
+    <v-row>
+      <v-col v-for="[key, value] in Object.entries(information)" :key="key">
+        <span @click="sortInformation(key)" class="change-info__header">
+          {{ value }}
+        </span></v-col
+      >
+      <v-col></v-col>
+    </v-row>
+    <v-row
+      v-for="(person, index) in persons"
       :key="index"
       class="d-flex justify-space-between align-center mb-2"
     >
-      <p class="desk__info ma-0">{{ person.name }} {{ person.surname }}</p>
-      <div class="d-flex align-center">
-        <p class="desk__area ma-0">Area: {{ person.area }}</p>
+      <v-col>
+        <p class="desk__info ma-0">
+          {{ person.personId }}
+        </p></v-col
+      >
+      <v-col>
+        <p class="desk__info ma-0">
+          {{ person.personName }}
+        </p></v-col
+      >
+      <v-col>
+        <p class="desk__info ma-0">
+          {{ person.personSurname }}
+        </p></v-col
+      >
+      <v-col class="d-flex justify-space-between">
+        <p class="desk__info ma-0">Work: {{ person.personIsWork }}</p>
+        <span
+          class="change-info__work mx-2 d-block"
+          :class="person.personIsWork ? 'background: green' : 'background: red'"
+          @click="$store.commit('changeWorkInfo', person)"
+        ></span
+      ></v-col>
+      <v-col
+        ><p class="desk__info ma-0">
+          Extra work: {{ person.personExtraWork }}
+        </p></v-col
+      >
 
-        <v-dialog v-model="dialog" persistent max-width="590">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn dark color="cyan" class="mx-4" v-bind="attrs" v-on="on">
-              <v-icon dark> mdi-pencil </v-icon>
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title class="text-h5">
-              Change information about person
-            </v-card-title>
-            <v-form
-              ref="form"
-              v-model="valid"
-              lazy-validation
-              class="desk-form ma-4"
-            >
-              <v-text-field
-                v-model="name"
-                :counter="10"
-                :rules="nameRules"
-                label="Name"
-                required
-              ></v-text-field>
+      <v-col
+        ><p class="desk__area ma-0">Area: {{ person.personArea }}</p></v-col
+      >
+      <v-col>
+        <div class="d-flex align-center">
+          <v-dialog v-model="dialog" persistent max-width="590">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                dark
+                color="cyan"
+                class="mx-4"
+                v-bind="attrs"
+                v-on="on"
+                @click="changePersonInfo(person)"
+              >
+                <v-icon dark> mdi-pencil </v-icon>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="text-h5">
+                Change information about person
+              </v-card-title>
+              <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+                class="desk-form ma-4"
+              >
+                <v-text-field
+                  v-model="person.personName"
+                  :counter="10"
+                  :rules="nameRules"
+                  label="Name"
+                  required
+                ></v-text-field>
 
-              <v-text-field
-                v-model="surname"
-                :counter="15"
-                :rules="surnameRules"
-                label="Surname"
-                required
-              ></v-text-field>
+                <v-text-field
+                  v-model="personChange.personSurname"
+                  :counter="15"
+                  :rules="surnameRules"
+                  label="Surname"
+                  required
+                ></v-text-field>
 
-              <v-select
-                v-model="select"
-                :items="$store.state.areas"
-                :rules="[(v) => !!v || 'Area is required']"
-                label="Area"
-                required
-              ></v-select>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="green darken-1" text @click="dialog = false">
-                  Disagree
-                </v-btn>
-                <v-btn color="green darken-1" text @click="validate">
-                  Agree
-                </v-btn>
-              </v-card-actions>
-            </v-form>
-          </v-card>
-        </v-dialog>
+                <v-select
+                  v-model="personChange.personArea"
+                  :items="$store.state.areas"
+                  :rules="[(v) => !!v || 'Area is required']"
+                  :label="'Area: ' + person.personArea"
+                  required
+                ></v-select>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="green darken-1" text @click="dialog = false">
+                    Disagree
+                  </v-btn>
+                  <v-btn
+                    color="green darken-1"
+                    text
+                    @click="validate(index, person)"
+                  >
+                    Agree
+                  </v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-dialog>
 
-        <v-btn color="error">DELETE</v-btn>
-      </div>
-    </div>
+          <v-btn color="error" @click="$store.commit('removePerson', index)"
+            >DELETE</v-btn
+          >
+        </div></v-col
+      >
+    </v-row>
   </v-col>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-@Component({})
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { IPersonInfo } from "@/services/models/IPerson-info.model";
+import { mapState } from "vuex";
+@Component({
+  computed: mapState(["information"]),
+})
 export default class ChangeInfoPerson extends Vue {
-  protected person = {
-    name: "",
-    surname: "",
-    area: "",
-    isHaveArea: true,
-    isActive: true,
-    extraHours: "",
+  @Prop({ required: true }) readonly persons!: any;
+  personChange: IPersonInfo = {
+    personId: 0,
+    personName: "",
+    personSurname: "",
+    personArea: true,
+    personIsWork: true,
+    personExtraWork: 0,
   };
-  dialog = false;
   valid = true;
-  name = "";
-  surname = "";
-  informationToChange = null;
+  dialog = false;
   nameRules = [
     (v: string) => !!v || "Name is required",
     (v: string) =>
@@ -98,33 +152,73 @@ export default class ChangeInfoPerson extends Vue {
   area = "";
   items = ["Item 1", "Item 2", "Item 3", "Item 4"];
   checkbox = false;
-
-  validate() {
-    (this.dialog = false),
-      (this.person = {
-        name: this.name,
-        surname: this.surname,
-        area: this.area,
-        isHaveArea: true,
-        isActive: true,
-        extraHours: "",
-      });
-    this.$store.state.staffInformation.push(this.person);
-    this.person = {
-      name: "",
-      surname: "",
-      area: "",
-      isHaveArea: true,
-      isActive: true,
-      extraHours: "",
+  changePersonInfo(item: IPersonInfo) {
+    this.personChange = {
+      personId: item.personId,
+      personName: item.personName,
+      personSurname: item.personSurname,
+      personArea: item.personArea,
+      personIsWork: item.personIsWork,
+      personExtraWork: item.personExtraWork,
     };
-
-    (this.$refs as { form: HTMLFormElement }).form.validate();
   }
-  reset() {
-    (this.$refs as { form: HTMLFormElement }).form.reset();
+  validate(id: number, item: IPersonInfo) {
+    if (item.personName !== null && item.personSurname !== null) {
+      this.personChange = {
+        personId: item.personId,
+        personName: item.personName,
+        personSurname: item.personSurname,
+        personArea: item.personArea,
+        personIsWork: item.personIsWork,
+        personExtraWork: item.personExtraWork,
+      };
+      console.log(id);
+      this.persons.push(this.personChange);
+      this.persons.splice(id, 1, this.personChange);
+      this.personChange = {
+        personId: 0,
+        personName: "",
+        personSurname: "",
+        personArea: true,
+        personIsWork: true,
+        personExtraWork: 0,
+      };
+      this.dialog = false;
+    } else {
+      (this.$refs as { form: HTMLFormElement }).form.validate();
+      this.dialog = false;
+    }
+  }
+  changeWorkInfo(item: boolean) {
+    item;
+    return;
+  }
+  sortInformation(sortedKey) {
+    this.persons.sort((a, b) => a[sortedKey] - b[sortedKey]);
   }
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.change-info {
+  &__work {
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+  }
+  &__header {
+    font-weight: 700;
+    font-size: 19px;
+    cursor: pointer;
+    width: 100%;
+    height: auto;
+    &:hover {
+      color: green;
+    }
+  }
+  .testBtn {
+    border-radius: 50%;
+  }
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,21 @@
+//идея vuex в том что мы описываем всю бизнесс логику в vuex
+
 import Vue from "vue";
 import Vuex from "vuex";
-
+import { IPersonInfo } from "@/services/models/IPerson-info.model";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     areas: ["absence", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    information: {
+      personId: "Number",
+      personName: "Name",
+      personSurname: "Surname",
+      personIsWork: "Is work?",
+      personExtraWork: "Extra work",
+      personArea: "Area",
+    },
     staffInformation: [
       {
         personId: 1,
@@ -13,27 +23,47 @@ export default new Vuex.Store({
         personSurname: "Surname1",
         personArea: 1,
         personIsWork: true,
-        personExtraWork: "",
+        personExtraWork: 33,
       },
       {
         personId: 2,
         personName: "Name2",
         personSurname: "Surname2",
-        personArea: "2",
+        personArea: 3,
         personIsWork: false,
-        personExtraWork: "",
+        personExtraWork: 22,
+      },
+      {
+        personId: 4,
+        personName: "dsaf",
+        personSurname: "DAFSS",
+        personArea: 2,
+        personIsWork: false,
+        personExtraWork: 222,
       },
     ],
   },
+
+  // мутации это то как мы меняем состояние(а состояние это данные в state)
   mutations: {
+    //только работа со стейтом
+    information(state) {
+      return state.information;
+    },
     removePerson(state, id: number) {
       return state.staffInformation.splice(id, 1);
     },
-    changeWorkInfo(_, person) {
+    changeWorkInfo(_, person: IPersonInfo) {
       return (person.personIsWork = !person.personIsWork);
     },
   },
-  actions: {},
-  modules: {},
-  getters: {},
+  actions: {
+    //любой ассинхроной штукой, если с запросами на сервере
+  },
+  modules: {
+    //стор можем делить на разные чанки (чанки это )
+  },
+  getters: {
+    //статическое поле что бы что то получить. разобраться
+  },
 });
