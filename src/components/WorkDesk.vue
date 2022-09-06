3c7853aafd20c7f805f3d03d6bc5704b5f91308b
Yevhen <shershen.yevhen@gmai>
2022-09-06 17:14:51 +0200

    import PersonInformation.vue in component WorkDesk.vue

--- src/components/WorkDesk.vue
+++ src/components/WorkDesk.vue
@@ -2,6 +2,7 @@
   <v-row>
     <AddPerson :persons="persons"></AddPerson>
     <ChangeInfoPerson :persons="persons"></ChangeInfoPerson>
+    <PersonInformation :persons="persons"></PersonInformation>
   </v-row>
 </template>
 
@@ -9,10 +10,12 @@
 import { Component, Vue } from "vue-property-decorator";
 import ChangeInfoPerson from "@/components/workdesk/ChangeInfoPerson.vue";
 import AddPerson from "@/components/workdesk/AddPerson.vue";
+import PersonInformation from "@/components/workdesk/PersonInformation.vue";
 @Component({
   components: {
     ChangeInfoPerson,
     AddPerson,
+    PersonInformation,
   },
 })
 export default class WorkDesk extends Vue {
