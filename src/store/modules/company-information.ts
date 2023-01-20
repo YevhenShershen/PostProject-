837efa741737ea34741a195250df0386c52c1ed7
Yevhen <shershen.yevhen@gmai>
2023-01-20 20:13:55 +0100

    create module company-information.ts

---
+++ src/store/modules/company-information.ts
@@ -0,0 +1,16 @@
+export const companyInformation = {
+  state: {
+    company: {
+      name: "Shershen-IT",
+      street: "Pikachu",
+      number: "888",
+      city: "New Star",
+      code: "88-888",
+      country: "Space",
+      phoneNumber: "888 888 888",
+      email: "shershen-it@gmail.com",
+      webPage: "shershe-it.com",
+      workTime: "Godziny pracy biura: 8:30 - 16:30",
+    },
+  },
+};
