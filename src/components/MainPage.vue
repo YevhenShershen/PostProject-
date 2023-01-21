1f3324e36f807ef8838809f530bdea9d683c3dea
Yevhen <shershen.yevhen@gmai>
2023-01-21 19:30:27 +0100

    create component MainPage.vue

---
+++ src/components/MainPage.vue
@@ -0,0 +1,20 @@
+<!-- vue-property-decorator -->
+<template>
+  <div>
+    <Description />
+    <OurOffer />
+    <OrderForm />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Description from "./main/Description.vue";
+import OurOffer from "./main/OurOffer.vue";
+import OrderForm from "./main/OrderForm.vue";
+@Component({
+  components: { Description, OurOffer, OrderForm },
+})
+export default class MainPage extends Vue {}
+</script>
+<style lang="scss" scoped></style>
