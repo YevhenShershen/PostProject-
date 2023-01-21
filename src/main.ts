978252a0389107861ad744e1a6100a28301fa14a
Yevhen <shershen.yevhen@gmai>
2023-01-21 19:26:43 +0100

    install  sass-loader

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 import "./scss/main.scss";
 Vue.config.productionTip = false;
 
