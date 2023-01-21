0c822d8e7385802cd81871140d43725406e3f254
Yevhen <shershen.yevhen@gmai>
2023-01-21 19:30:00 +0100

    create component Slider.vue

---
+++ src/components/main/Slider.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-carousel hide-delimiters style="height: 355px">
+    <v-carousel-item
+      v-for="(item, i) in elem"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Slider extends Vue {
+  elem = [
+    {
+      src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+    },
+    {
+      src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+    },
+    {
+      src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+    },
+    {
+      src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+    },
+  ];
+}
+</script>
