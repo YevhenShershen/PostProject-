4e962f4e1bccad36ee3ee53dc1786d3d464a47b5
Yevhen <shershen.yevhen@gmai>
2023-01-26 18:30:11 +0100

    learn "vuex-class" + typescript (create type RootState and use it)

--- src/store/todos/todos.ts
+++ src/store/todos/todos.ts
@@ -1,4 +1,4 @@
-import { TodoState, Todo } from "./types";
+import { RootState, TodoState, Todo } from "./types";
 import { GetterTree, MutationTree, ActionTree } from "vuex";
 export const state: TodoState = {
   todos: [
@@ -16,8 +16,9 @@ export const state: TodoState = {
     },
   ],
 };
-export const getters: GetterTree<TodoState, any> = {
-  todos: (state) => state.todos.filter((todo) => !todo.checked),
+export const getters: GetterTree<TodoState, RootState> = {
+  todos: (state, getters, rootState) =>
+    state.todos.filter((todo) => !todo.checked),
   dones: (state) => state.todos.filter((todo) => todo.checked),
 };
 export const mutations: MutationTree<TodoState> = {
@@ -28,14 +29,14 @@ export const mutations: MutationTree<TodoState> = {
     todo.checked = !todo.checked;
   },
 };
-export const actions: ActionTree<TodoState, any> = {
-  addTodoAsync({ commit }, id) {
+export const actions: ActionTree<TodoState, RootState> = {
+  addTodoAsync({ commit, rootState }, id) {
     fetch("https://jsonplaceholder.typicode.com/todos/" + id)
       .then((data) => data.json())
       .then((item) => {
         const todo: Todo = {
           checked: false,
-          text: item.title,
+          text: rootState.login.user + ": " + item.title,
         };
         commit("addTodo", todo);
       });
