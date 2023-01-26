0cf43a9419e0fb15bbd8d7cbed111b53b42e8974
Yevhen <shershen.yevhen@gmai>
2023-01-26 17:52:22 +0100

    store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,12 @@
 import Vue from "vue";
-import Vuex, { StoreOptions } from "vuex";
+import Vuex from "vuex";
 import { companyInformation } from "@/store/company-information/company-information";
 import { employees } from "@/store/employees/employees";
 import { navigation } from "@/store/navigation/navigation";
 import { todos } from "@/store/todos/todos";
-import { state, getters, mutations, actions } from "@/store/todos/todos";
+import { login } from "@/store/todos/login";
 Vue.use(Vuex);
-const store: StoreOptions<any> = {
-  state: {},
-  modules: { companyInformation, employees, navigation, todos },
-};
-export default new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions,
+
+export const store = new Vuex.Store({
+  modules: { companyInformation, employees, navigation, todos, login },
 });
