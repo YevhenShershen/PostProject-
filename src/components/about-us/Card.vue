baf0fac9cd7ba48e9b98adc9a67f7b6f3765578a
Yevhen <shershen.yevhen@gmai>
2023-02-07 18:57:53 +0100

    Create and implement component Card

---
+++ src/components/about-us/Card.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-col
+    xl="4"
+    lg="4"
+    md="5"
+    sm="12"
+    xs="12"
+    class="d-flex justify-space-around"
+  >
+    <div class="card d-flex flex-column align-center">
+      <p class="card_company-name">
+        {{ CompanyInformation.name }}
+      </p>
+      <img
+        alt=""
+        class="card_personal-photo"
+        src="~@/assets/images/employees/employee.jpg"
+      />
+      <p class="card_personal-name">{{ employee.name }}</p>
+      <p class="card_personal-profession">{{ profession }}</p>
+      <p class="card_personal-information">ID:{{ employee.id }}</p>
+      <p class="card_personal-information">EMAIL:{{ employee.email }}</p>
+      <p class="card_personal-information">TEL:{{ employee.phone }}</p>
+    </div>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { Employee, ICompanyInformation } from "@/store/types";
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { mapGetters } from "vuex";
+@Component({
+  computed: {
+    ...mapGetters({
+      CompanyInformation: "companyInformation/COMPANY_INFORMATION",
+    }),
+  },
+})
+export default class PersonInfo extends Vue {
+  @Prop({ required: true }) readonly employee!: Employee;
+  public profession = "programista";
+  public CompanyInformation!: ICompanyInformation;
+}
+</script>
+<style lang="scss" scoped>
+.card {
+  background-image: url("~@/assets/images/employees/card.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  border-radius: 15px;
+  min-width: 374px;
+  &_company-name {
+    margin-top: 50px;
+    margin-bottom: 30px;
+  }
+
+  &_personal-photo {
+    border-radius: 100%;
+    box-shadow: 0 0 0 3px #3a3d40, 0 0 13px #3a3d40;
+    width: 120px;
+  }
+
+  &_personal-name {
+    margin-top: 30px;
+  }
+
+  &_personal-profession {
+  }
+
+  &_personal-information {
+    border-bottom: 2px dotted #3a3d40;
+    width: 280px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+.card_personal-information:last-child {
+  margin-bottom: 80px;
+}
+</style>
