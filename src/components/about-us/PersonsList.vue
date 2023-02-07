9fec1006d658d4ae58f13fa3b3af51a4ff3dc78e
Yevhen <shershen.yevhen@gmai>
2023-02-07 19:13:10 +0100

    create and implement component PersonsList

---
+++ src/components/about-us/PersonsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <Card
+      v-for="(item, id) in employeesList"
+      :key="`card-${id}`"
+      :employee="item"
+    ></Card>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Card from "@/components/about-us/Card.vue";
+import { mapGetters } from "vuex";
+import { Employees } from "@/store/types";
+@Component({
+  components: { Card },
+  computed: {
+    ...mapGetters({
+      employeesList: "employees/EMPLOYEES",
+    }),
+  },
+})
+export default class PersonsList extends Vue {
+  public employeesList!: Employees;
+}
+</script>
+<style lang="scss" scoped></style>
