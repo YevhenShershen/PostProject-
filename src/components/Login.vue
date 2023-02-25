25f3c4ebd27768d356a19b7060641c7826e6d27c
Yevhen <shershen.yevhen@gmai>
2023-02-25 20:48:58 +0100

    implement Login component and create component AdminPanel.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,36 +7,60 @@
       <v-card-text>
         <v-form>
           <v-text-field
-            label="Username"
+            :label="isLogin ? 'Mail' : 'incorrect mail'"
             prepend-icon="mdi-account-circle"
+            v-model.trim="loginData.mail"
           ></v-text-field>
           <v-text-field
             :type="showPassword ? 'test' : 'password'"
-            label="Password"
+            :label="isLogin ? 'Password' : 'incorrect password'"
             prepend-icon="mdi-lock"
             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            v-model.trim="loginData.password"
             @click:append="showPassword = !showPassword"
           ></v-text-field>
         </v-form>
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn color="success">Register</v-btn>
         <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
+
+        <v-btn color="info" @click="checkLoginData(loginData)">Login</v-btn>
       </v-card-actions>
     </v-card>
   </v-main>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
-export default Vue.extend({
-  name: "Login",
-
-  data: () => ({
-    showPassword: false,
-  }),
-});
+import { Component, Vue } from "vue-property-decorator";
+interface IAdmin {
+  mail: string;
+  password: string;
+}
+@Component({})
+export default class HeaderApp extends Vue {
+  name: any = "Login";
+  showPassword: any = false;
+  isLogin: boolean | string = true;
+  admin: IAdmin = {
+    mail: "admin@mail.com",
+    password: "qwerty123A!",
+  };
+  loginData: IAdmin = {
+    mail: "",
+    password: "",
+  };
+  checkLoginData(arg: IAdmin): void {
+    if (this.admin.mail == arg.mail && this.admin.password == arg.password) {
+      this.isLogin = true;
+      this.$router.push("/admin");
+    } else {
+      this.loginData = {
+        mail: "",
+        password: "",
+      };
+      this.isLogin = false;
+    }
+  }
+}
 </script>
---
+++ src/components/admin-panel/AdminPanel.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><h1>ADMIN PANEL</h1></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({})
+export default class AdminPanel extends Vue {}
+</script>
+<style lang="scss" scoped></style>
