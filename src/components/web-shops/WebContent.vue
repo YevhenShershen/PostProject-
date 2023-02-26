5f1cd1f51dbaa2367c10937119e778509f18abfc
Yevhen <shershen.yevhen@gmai>
2023-02-26 19:54:15 +0100

    create component WebContent.vue and create initial functionality

---
+++ src/components/web-shops/WebContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-col>{{ categoryList }} </v-col>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class WebContent extends Vue {
+  @Prop({ required: true }) readonly categoryList!: any[];
+}
+</script>
+<style lang="scss" scoped></style>
