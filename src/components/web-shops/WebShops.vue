fc741baffc7de0c80a2f9f4470d1d65731cdcfce
Yevhen <shershen.yevhen@gmai>
2023-02-26 19:54:29 +0100

    create component WebShops.vue and create initial functionality

--- src/components/web-shops/WebShops.vue
+++ src/components/web-shops/WebShops.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>WEB SHOPS</h1>
+  <div>
+    <h1>WEB SHOPS LIST</h1>
+    <WebCategoryList
+      v-for="(category, index) in categories"
+      :category="category"
+      :key="`${index}-web-category`"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component
-export default class WebShops extends Vue {}
+import WebCategoryList from "./WebCategoryList.vue";
+import categories from "@/services/enums/categories";
+@Component({
+  components: { WebCategoryList },
+})
+export default class WebShops extends Vue {
+  public categories = categories;
+}
 </script>
 <style lang="scss" scoped></style>
